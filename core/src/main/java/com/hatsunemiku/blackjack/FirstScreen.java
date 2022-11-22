4653a4404bd851bc410b9c95ff339f5ea525fb6d
Alessandro Schwaiger <aless2003@gmx.at>
2022-11-22 01:10:22 +0100

    Created ready to go Project
    Android setup latest version
    Lwjgl3 Desktop Version
    IOS support added

---
+++ core/src/main/java/com/hatsunemiku/blackjack/FirstScreen.java
@@ -0,0 +1,49 @@
+package com.hatsunemiku.blackjack;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import de.eskalon.commons.screen.ManagedScreen;
+
+/** First screen of the application. Displayed after the application is created. */
+public class FirstScreen extends ManagedScreen {
+
+	@Override
+	protected void create() {
+
+	}
+
+	@Override
+	public void show() {
+		// Prepare your screen here.
+	}
+
+	@Override
+	public void render(float delta) {
+		// Draw your screen here. "delta" is the time since last render in seconds.
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// Resize your screen here. The parameters represent the new window size.
+	}
+
+	@Override
+	public void pause() {
+		// Invoked when your application is paused.
+	}
+
+	@Override
+	public void resume() {
+		// Invoked when your application is resumed after pause.
+	}
+
+	@Override
+	public void hide() {
+		// This method is called when another screen replaces this one.
+	}
+
+	@Override
+	public void dispose() {
+		// Destroy screen's assets here.
+	}
+}
